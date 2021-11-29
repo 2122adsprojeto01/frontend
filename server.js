962ed4c8bca51a0bfa8eacf4053d1bd844ced6e7
lunayue <sicpo1@iscte-iul.pt>
2021-11-29 12:01:16 +0000

    bridge should be working now????

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const bodyParser = require("body-parser");
-const router = express.Router();
+const router_client = express.Router();
+const router_java_get_stuff = express.Router();
+const router_java_send_stuff = express.Router();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -11,6 +13,9 @@ let docker_clients = []
 let client_id = 0
 let clients = new Map()
 
+app.set('view engine', 'pug');
+app.set('views', './views');
+
 app.listen(port, () => {
   console.log('ADS App listening on port ' + port)
 })
@@ -19,35 +24,57 @@ app.get('/', (req, res) => {
     res.render("index", { title: "Hey", message: "Hello there!" });
 })
 
+app.get('/curatorauth', (req, res) => {
+    res.render("curatorauth");
+});
+
 app.get('/about', (req, res) => {
     res.render("about", { title: "Hey", message: "Hello there!" });
 })
 
-router.get('/server_client', (req, res) => {
-  docker_clients.unshift(res)
-  res.setTimeout(20000, () => {
-    res.send("Timed out")
-    docker_clients.splice(docker_clients.indexOf(res), 1)
-  })
-})
 
-router.post("/server_client_post", (req, res) => {
-  let id = parseInt(req.body.id)
-  clients.get(id).send(req.body.data)
-  clients.delete(id)
-  res.send("Post sent")
-});
+app.use("/server_client", router_java_get_stuff);
+router_java_get_stuff.get('*', handle_java_connection)
+router_java_get_stuff.post('*', handle_java_connection)
 
-app.set('view engine', 'pug');
-app.set('views', './views');
+function handle_java_connection(req, res) {
+    console.log("hello?");
+    docker_clients.unshift(res)
+    res.setTimeout(20000, () => {
+        res.send("Timed out")
+        docker_clients.splice(docker_clients.indexOf(res), 1)
+    })
+}
 
-app.use("/boop", router);
 
-router.get('*', handle_client_request)
 
-router.post('*', handle_client_request)
+app.use("/server_client_post", router_java_send_stuff);
+router_java_send_stuff.get('*', handle_java_recieve)
+router_java_send_stuff.post('*', handle_java_recieve)
 
-function handle_client_request(req, res){
+function handle_java_recieve(req, res) {
+    console.log("got stuff?");
+    let id = parseInt(req.body.id)
+    clients.get(id).send(req.body.data)
+    clients.delete(id)
+    res.send("Post sent")
+}
+
+
+
+
+
+app.use("/viewer", router_client);
+app.use("/curator", router_client);
+app.use("/editor", router_client);
+app.use("/boop", router_client);
+app.use("/checkCuratorIsValid", router_client);
+
+router_client.get('*', handle_client_request)
+router_client.post('*', handle_client_request)
+
+function handle_client_request(req, res) {
+    console.log(req.body)
   if (docker_clients.length > 0){
     console.log(docker_clients.length)
     client_id += 1
